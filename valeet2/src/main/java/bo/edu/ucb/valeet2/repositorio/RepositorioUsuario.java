44d474b036623b6764168e747c3767b4b4f36d49
ignacio Salgado <ignaciosalgado30@gmail.com>
2019-10-29 19:35:05 -0400

    Subiendo archivos

--- valeet2/src/main/java/bo/edu/ucb/valeet2/repositorio/RepositorioUsuario.java
+++
@@ -1,8 +0,0 @@
-package bo.edu.ucb.valeet2.repositorio;
-
-import bo.edu.ucb.valeet2.modelo.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-public interface RepositorioUsuario extends JpaRepository<Usuario, Long>  {
-}
