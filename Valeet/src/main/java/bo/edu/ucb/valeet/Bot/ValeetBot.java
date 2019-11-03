71ad09aa97ba6588c5e9681173d940097a952059
napo <napocuba@gmail.com>
2019-11-03 14:26:27 -0400

    mejorado de menu

--- Valeet/src/main/java/bo/edu/ucb/valeet/Bot/ValeetBot.java
+++ Valeet/src/main/java/bo/edu/ucb/valeet/Bot/ValeetBot.java
@@ -8,7 +8,7 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 public class ValeetBot extends TelegramLongPollingBot {
 
     public void onUpdateReceived(Update update) {
-        String aux,usser,placa,garage, nit,zona,espacios,direccion;
+        String aux,usser,pass,email,placa,garage, nit,zona,espacios,direccion,name,lastname,secondlastname;
 
         SendMessage message = new SendMessage ();
        /* System.out.println(update.getMessage().getText());
@@ -17,55 +17,94 @@ public class ValeetBot extends TelegramLongPollingBot {
 
         */
         String command = update.getMessage ().getText ();
-        if (command.equals ( "/parqueo" )) {
-            System.out.println ( "Donde necesitas tu parqueo?" );
-            message.setText ( "Donde necesitas tu parqueo?" );
 
+
+        if (command.equals ( "/login" )) {
+            System.out.println ( "favor ingrese su email" );
+            message.setText ( "favor ingrese su email" );
+            email=update.getMessage ().getText ();
+            System.out.println ( "favor ingrese su password" );
+            message.setText ( "favor ingrese su password" );
+            pass=update.getMessage ().getText ();
+            if (email.equals ( email )&&pass.equals ( pass )){
+                System.out.println ( "que opcion desea" );
+                message.setText ( "que opcion desea" );
+              if (command.equals ( "/parqueo" )){
+                  System.out.println ( "Donde desa su parqueo?" );
+              }
+              if (command.equals ( "/registrarme" )) {
+                    System.out.println ( "Deseas registrar un auto o un parqueo?" );
+                    message.setText ( "Deseas registrar un auto o un parqueo? o desea eliminar" );
+                    if (command.equals ( "/auto" )) {
+                        System.out.println ( "Favor de ingresar tu nombre de usuario " );
+                        message.setText ( "Favor de ingresar tu nombre de usuario");
+                        usser=update.getMessage ().getText ();
+                        message.setText ( "Favor de ingresar tu nombre");
+                        name=update.getMessage ().getText ();
+                        message.setText ( "Favor de ingresar tu apellido paterno");
+                        lastname=update.getMessage ().getText ();
+                        message.setText ( "Favor de ingresar tu apellido materno");
+                        secondlastname=update.getMessage ().getText ();
+                        message.setText ( "Favor de ingresar tu correo electronico");
+                        email=update.getMessage ().getText ();
+                        message.setText ( "Favor de ingresar tu password");
+                        pass=update.getMessage ().getText ();
+                        message.setText ( "Favor ingrese su numero de placa");
+                        placa=update.getMessage ().getText ();
+                        message.setText ( " favor ingrese su nit");
+                        nit= update.getMessage ().getText ();
+                        System.out.println ( name+lastname+secondlastname+usser+email+pass+placa+nit+pass );
+
+                    }
+                    if (command.equals ( "/garage" )) {
+
+                        message.setText (  "Favor de ingresar tu nombre de usuario ");
+                        usser = update.getMessage ().getText ();
+                        message.setText ( "Favor de ingresar tu nombre");
+                        name=update.getMessage ().getText ();
+                        message.setText ( "Favor de ingresar tu apellido paterno");
+                        lastname=update.getMessage ().getText ();
+                        message.setText ( "Favor de ingresar tu apellido materno");
+                        secondlastname=update.getMessage ().getText ();
+                        message.setText ( "Favor de ingresar tu correo electronico");
+                        email=update.getMessage ().getText ();
+                        message.setText ( "Favor de ingresar tu password");
+                        pass=update.getMessage ().getText ();
+                        message.setText ( "Favor ingresa tu zona ");
+                        zona = update.getMessage ().getText ();
+                        message.setText ( "Favor ingresa la cantidad de  espacios en tu garage");
+                        espacios = update.getMessage ().getText ();
+                        message .setText ( "Favor ingrese su nit ");
+                        nit = update.getMessage ().getText ();
+                        System.out.println ( name+lastname+secondlastname+usser+email+pass+zona+espacios+nit );
+                        System.out.println ( "Favor de ingresar tu direccion completa" );
+                        message.setText (  "Favor de ingresar tu direccion completa" );
+                        direccion = update.getMessage ().getText ();
+
+                    }
+                    if (command.equals ( "/eliminar" )){
+                        if (command.equals ( "/garage")){
+                            System.out.println ( "Favor de ingresar tu nombre de usuario  y zona ejemplo(napo-miraflores)" );
+                            message.setText ( "Favor de ingresar tu nombre de usuario  y zona ejemplo(napo-miraflores)" );
+                            aux = update.getMessage ().getText ();
+                            System.out.println ( aux );
+                        }
+                        if (command.equals ( "/auto" )){
+                            System.out.println ( "Favor de ingresar tu nombre de usuario  y placa ejemplo(napo-4231pfs)" );
+                            message.setText ( "Favor de ingresar tu nombre de usuario  y zona ejemplo(napo-4231pfs)" );
+                            aux = update.getMessage ().getText ();
+                            System.out.println ( aux );
+                        }
+                    }
+                }
+
+            }else {
+                System.out.println ( "email o pass incorrecto intente de nuevo" );
+                message.setText ( "email o pass incorrecto intente de nuevo" );
+            }
         }
 
-        if (command.equals ( "/registrarme" )) {
-            System.out.println ( "Deseas registrar un auto o un parqueo?" );
-            message.setText ( "Deseas registrar un auto o un parqueo? o desea eliminar" );
-            if (command.equals ( "/auto" )) {
-                System.out.println ( "Favor de ingresar tu nombre de usuario , tu numero de placa y nit ejemplo(napo-4231tfx-123123)" );
-                message.setText ( "Favor de ingresar tu nombre de usuario ,tu numero de placa y nit ejemplo(napo-4231tfx-123123)" );
-                aux = update.getMessage ().getText ();
-                System.out.println ( aux );
-                String[] parts = aux.split ( "-");
-                usser = parts[0];
-                placa = parts[1];
-                nit = parts[2];
-            }
-            if (command.equals ( "/garage" )) {
-                System.out.println ( "Favor de ingresar tu nombre de usuario , tu zona  ,cantidad de  espacios ,  y nit ejemplo(napo-miraflores-50-23123)" );
-                message.setText (  "Favor de ingresar tu nombre de usuario , tu zona  ,cantidad de  espacios ,  y nit ejemplo(napo-miraflores-50-23123)" );
-                aux = update.getMessage ().getText ();
-                System.out.println ( aux );
-                String[] parts = aux.split ( "-");
-                usser = parts[0];
-                zona = parts[1];
-                espacios= parts[2];
-                nit =parts[3];
-                System.out.println ( "Favor de ingresar tu direccion completa" );
-                message.setText (  "Favor de ingresar tu direccion completa" );
-                direccion = update.getMessage ().getText ();
 
-            }
-            if (command.equals ( "/eliminar" )){
-                if (command.equals ( "/garage")){
-                System.out.println ( "Favor de ingresar tu nombre de usuario  y zona ejemplo(napo-miraflores)" );
-                message.setText ( "Favor de ingresar tu nombre de usuario  y zona ejemplo(napo-miraflores)" );
-                aux = update.getMessage ().getText ();
-                System.out.println ( aux );
-            }
-            if (command.equals ( "/auto" )){
-                System.out.println ( "Favor de ingresar tu nombre de usuario  y placa ejemplo(napo-4231pfs)" );
-                message.setText ( "Favor de ingresar tu nombre de usuario  y zona ejemplo(napo-4231pfs)" );
-                aux = update.getMessage ().getText ();
-                System.out.println ( aux );
-            }
-            }
-        }
 
 
 
