d372b502801314118daf90d542e7804d3726eddb
napo <napocuba@gmail.com>
2019-11-03 04:02:11 -0400

    opciones del chatbot

--- Valeet/src/main/java/bo/edu/ucb/valeet/Bot/ValeetBot.java
+++ Valeet/src/main/java/bo/edu/ucb/valeet/Bot/ValeetBot.java
@@ -8,23 +8,79 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 public class ValeetBot extends TelegramLongPollingBot {
 
     public void onUpdateReceived(Update update) {
-        System.out.println(update.getMessage().getText());
-        SendMessage message = new SendMessage();
+        String aux,usser,placa,garage, nit,zona,espacios,direccion;
+
+        SendMessage message = new SendMessage ();
+       /* System.out.println(update.getMessage().getText());
+
         message.setText(update.getMessage().getText());
 
-        message.setChatId(update.getMessage().getChatId());
+        */
+        String command = update.getMessage ().getText ();
+        if (command.equals ( "/parqueo" )) {
+            System.out.println ( "Donde necesitas tu parqueo?" );
+            message.setText ( "Donde necesitas tu parqueo?" );
+
+        }
+
+        if (command.equals ( "/registrarme" )) {
+            System.out.println ( "Deseas registrar un auto o un parqueo?" );
+            message.setText ( "Deseas registrar un auto o un parqueo? o desea eliminar" );
+            if (command.equals ( "/auto" )) {
+                System.out.println ( "Favor de ingresar tu nombre de usuario , tu numero de placa y nit ejemplo(napo-4231tfx-123123)" );
+                message.setText ( "Favor de ingresar tu nombre de usuario ,tu numero de placa y nit ejemplo(napo-4231tfx-123123)" );
+                aux = update.getMessage ().getText ();
+                System.out.println ( aux );
+                String[] parts = aux.split ( "-");
+                usser = parts[0];
+                placa = parts[1];
+                nit = parts[2];
+            }
+            if (command.equals ( "/garage" )) {
+                System.out.println ( "Favor de ingresar tu nombre de usuario , tu zona  ,cantidad de  espacios ,  y nit ejemplo(napo-miraflores-50-23123)" );
+                message.setText (  "Favor de ingresar tu nombre de usuario , tu zona  ,cantidad de  espacios ,  y nit ejemplo(napo-miraflores-50-23123)" );
+                aux = update.getMessage ().getText ();
+                System.out.println ( aux );
+                String[] parts = aux.split ( "-");
+                usser = parts[0];
+                zona = parts[1];
+                espacios= parts[2];
+                nit =parts[3];
+                System.out.println ( "Favor de ingresar tu direccion completa" );
+                message.setText (  "Favor de ingresar tu direccion completa" );
+                direccion = update.getMessage ().getText ();
+
+            }
+            if (command.equals ( "/eliminar" )){
+                if (command.equals ( "/garage")){
+                System.out.println ( "Favor de ingresar tu nombre de usuario  y zona ejemplo(napo-miraflores)" );
+                message.setText ( "Favor de ingresar tu nombre de usuario  y zona ejemplo(napo-miraflores)" );
+                aux = update.getMessage ().getText ();
+                System.out.println ( aux );
+            }
+            if (command.equals ( "/auto" )){
+                System.out.println ( "Favor de ingresar tu nombre de usuario  y placa ejemplo(napo-4231pfs)" );
+                message.setText ( "Favor de ingresar tu nombre de usuario  y zona ejemplo(napo-4231pfs)" );
+                aux = update.getMessage ().getText ();
+                System.out.println ( aux );
+            }
+            }
+        }
+
+
+
+        message.setChatId ( update.getMessage ().getChatId () );
         try {
-            execute(message);
+            execute ( message );
         } catch (TelegramApiException e) {
-            e.printStackTrace();
+            e.printStackTrace ();
         }
     }
-
     public String getBotUsername() {
-        return "Valeet_Bot";
+        return "NeroBot";
     }
 
     public String getBotToken() {
-        return  "941105687:AAGRRvJsE2_3beZ7k5-xXqC-CgJR4e-T2rQ";
+        return  "992793333:AAGEKSr1njNT7HfW_ty4cmucVNDoTQIghdk";
     }
 }
